c9ce516dfe1ec29aecb85ef14fdc0ecd0230d411
knugraha-arn <knugraha@arranetwork.com>
2026-06-06 20:23:23 +0700

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
+        redirectTo: `https://avira.arranetwork.com/auth/callback`,
         queryParams: {
           hd: 'arranetwork.com',
         },
